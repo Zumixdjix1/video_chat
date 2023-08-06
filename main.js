6453f9ba970ea86b75458b3a0c0fcd72f806c734
Zumixdjix1 <110113056+Zumixdjix1@users.noreply.github.com>
2023-08-06 11:35:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-
+// Javascript
+// Note that to avoid browser-compatibility issues, this sample uses the import command to import the SDK and the vite to package the JS file.
+import AC from 'agora-chat'
