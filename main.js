4ebee662a0d5cbd018422b7f46e9ac820626b1bc
Zumixdjix1 <110113056+Zumixdjix1@users.noreply.github.com>
2023-08-06 12:48:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// Javascript
-// Note that to avoid browser-compatibility issues, this sample uses the import command to import the SDK and the vite to package the JS file.
-import AC from 'agora-chat'
+// Typescript
+import AC, { AgoraChat } from 'agora-chat'
+
 
 
 
